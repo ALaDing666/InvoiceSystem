592b35aea9f1f15e6dec5508ac757ac9da7fb199
ALaDing666 <774332622@qq.com>
2021-03-16 22:30:05 +0800

    first commit

---
+++ server/util/dbconfig.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql')
+module.exports = {
+  //数据库配置
+  config: {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '774332622lzq',
+    database: 'mydb1'
+  },
+  //连接数据库，使用mysql的连接池连接方式
+  //连接池对象
+  sqlConnect: function(sql, sqlArr, callBack) {
+    var pool = mysql.createPool(this.config)
+    pool.getConnection((err, conn) => {
+      console.log('12345')
+      if(err) {
+        console.log('连接失败')
+        return
+      }
+      //事件驱动回调
+      conn.query(sql, sqlArr, callBack)
+      //释放连接
+      conn.release()
+    })
+  }
+}
