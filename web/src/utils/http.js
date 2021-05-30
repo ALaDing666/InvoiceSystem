d1a689ae8acd8575481320a26ef8c37370cc0add
ALaDing666 <774332622@qq.com>
2021-05-30 22:41:01 +0800

    提交http.js

---
+++ web/src/utils/http.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: 'http://192.168.0.9:3000/api',
+  timeout: 10000
+})
+
+http.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  if (sessionStorage.getItem('token')) {
+    console.log('传token给后端......')
+    config.headers.authorization = sessionStorage.getItem('token')
+  }
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+export default http
